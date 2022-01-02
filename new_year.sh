e0b59f849ec611095730a24a3e2f289d5ddd3141
ddch42 <85430824+dch42@users.noreply.github.com>
2022-01-01 19:36:54 -0600

    add opts

--- new_year.sh
+++ new_year.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-CURRENT_YEAR=`date +%Y`
-luck="good bad"
-
+declare CURRENT_YEAR=`date +%Y`
+declare luck="good bad"
 declare -i roll=$(( $RANDOM % 2 + 1 ))
+declare optstring=":he"
+
+function usage {
+    echo "Usage: $(basename $0) [-he]" 2>&1
+    echo "  -h  show usage info"
+    echo "  -e  easy mode, increase odds of having a good year"
+    exit 1
+}
+
+while getopts ${optstring} arg; 
+do
+    case "${arg}" in
+        h) usage ;;
+        e) roll=$(( $RANDOM % 7 + 1 )) ;;
+        ?) echo "Invalide option: -${OPTARG}" && usage ;;
+    esac
+done
 
 [ $roll = 1 ] && 
-echo "$CURRENT_YEAR will be a ${luck%% *} year!" || 
-echo "$CURRENT_YEAR will be a ${luck##* } year!"
+echo "$CURRENT_YEAR will be a ${luck##* } year!" || 
+echo "$CURRENT_YEAR will be a ${luck%% *} year!"
